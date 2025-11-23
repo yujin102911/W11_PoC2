9ef0f8679b931625b8f0143b54ec28202873da1a
summer-yeoleum <indeedselfharm@gmail.com>
2025-11-23 16:43:00 +0900

    코드수정

--- W11_PoC2/Assets/Scripts/EnemyMove/HorizontalEnemy.cs
+++ W11_PoC2/Assets/Scripts/EnemyMove/HorizontalEnemy.cs
@@ -63,15 +63,6 @@ public class HorizontalEnemy : MonoBehaviour
         }
     }
 
-    // 2D 트리거 충돌 감지 및 데미지 로직
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("플레이어에게 데미지를 줍니다: " + other.gameObject.name);
-        }
-    }
-
     // Scene 뷰에 이동 경로 시각화 (기즈모)
     private void OnDrawGizmos()
     {
--- W11_PoC2/Assets/Scripts/EnemyMove/RotationEnemy.cs
+++ W11_PoC2/Assets/Scripts/EnemyMove/RotationEnemy.cs
@@ -16,14 +16,4 @@ public class RotationEnemy : MonoBehaviour
         transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
     }
 
-    // 2D 트리거 충돌 감지 함수
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        // 충돌 대상이 "Player" 태그인지 확인
-        if (other.gameObject.CompareTag("Player"))
-        {
-            // 데미지 로그 출력
-            Debug.Log("플레이어에게 데미지를 줍니다: " + other.gameObject.name);
-        }
-    }
 }
